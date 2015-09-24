5e510aca6cb6e9b5d3eecd0f1032b4f167d6f3bd
Simon Scarfe <simon.scarfe@gmail.com>
2015-09-25 00:34:00 +0100

    removed noddy test code

--- js/routes/index.js
+++ js/routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' });
-});
-
 module.exports = router;
