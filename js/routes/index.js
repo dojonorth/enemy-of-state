329a8b422beb522c1bb861d386a910b7d495c861
Simon Scarfe <simon.scarfe@gmail.com>
2015-10-02 02:42:32 +0100

    the start of a front end

--- js/routes/index.js
+++ js/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var EventStore = require('../lib/EventStore');
+var BeebStream = require('../lib/aggregates/BeebStream');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    var streamId = req.query.stream || 0;
+
+    var es1 = new EventStore();
+
+    es1.save({event: 'BeebAdded', data: {date: new Date(), content: 'welcome to beebstream'}})
+    es1.save({event: 'BeebAdded', data: {date: new Date(), content: 'awesome'}})
+    es1.save({event: 'BeebAdded', data: {date: new Date(), content: 'yay'}})
+
+    var es2 = new EventStore();
+
+    es2.save({event: 'BeebAdded', data: {date: new Date(), content: 'welcome to beebstream2'}})
+    es2.save({event: 'BeebAdded', data: {date: new Date(), content: 'awesome'}})
+    es2.save({event: 'BeebAdded', data: {date: new Date(), content: 'yay'}})
+
+    var streams = [es1, es2];
+    var es = streams[streamId];
+
+    var beebStream = new BeebStream(es.getAllEvents());
+
+    res.render('index', { title: 'Beebr', beebs: beebStream.toArray()});
 });
 
 module.exports = router;
