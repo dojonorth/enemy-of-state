e5f398ac18e6285ee702289dfcabd7a5812d3993
Simon Scarfe <simon.scarfe@gmail.com>
2015-10-08 01:19:18 +0100

    added first task and some IDs to our test aggregates

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# enemy-of-state
-Repository for BBC Dojo on coding without state scheduled for October 9th, 2015
+# Enemy of State
+
+This dojo is focussed around event sourcing. In the JS subfolder, you will find a very simple node app for a new anonymous twitter clone called Beebr.
+
+To get it set up, `cd js` and then `npm install`. Once installed, you should be able to `npm start` and visit http://localhost:3000 to see the basic clone.
+
+Looking through the code, you will hopefully see that these lists are not initially objects representing individual posts, but rather a series of events which are being sent into aggregators to derive models before passing them into a very simple view.
+
+## Task 1 - Delete a Beeb
+
+The current event aggregation naïvely only responds to BeebAdded events, now we want to make it aware of BeebDeleted events. The event should probably only consist of the event name with a payload of the ID you wish to delete.
+
+This can be exercised in two ways - first, there is our BeebStreamSpec file - probably the way to get the quickest feedback on your aggregation. And then there is the front end manifestation of this, if you add some example eventstores to the controller, you should be able to restart your node instance to see 'real life' versions of this code.
+
+## Task 2 - 
--- js/test/aggregates/BeebStreamSpec.js
+++ js/test/aggregates/BeebStreamSpec.js
@@ -6,31 +6,31 @@ describe('BeebStream', function () {
     it('should get generated from an event store', function () {
         var eventStore = new EventStore();
 
-        eventStore.save({event: 'BeebAdded', data: {date: new Date(), content: 'welcome to beebstream'}})
-        eventStore.save({event: 'BeebAdded', data: {date: new Date(), content: 'awesome'}})
-        eventStore.save({event: 'BeebAdded', data: {date: new Date(), content: 'yay'}})
+        eventStore.save({event: 'BeebAdded', data: {id: 1, date: new Date(), content: 'welcome to beebstream'}})
+        eventStore.save({event: 'BeebAdded', data: {id: 2, date: new Date(), content: 'awesome'}})
+        eventStore.save({event: 'BeebAdded', data: {id: 3, date: new Date(), content: 'yay'}})
 
         var beebStream = new BeebStream(eventStore.getAllEvents());
 
         assert.deepEqual([
-            {date: new Date(), content: 'welcome to beebstream'},
-            {date: new Date(), content: 'awesome'},
-            {date: new Date(), content: 'yay'}
+            {id: 1, date: new Date(), content: 'welcome to beebstream'},
+            {id: 2, date: new Date(), content: 'awesome'},
+            {id: 3, date: new Date(), content: 'yay'}
         ], beebStream.toArray());
     });
 
     it('should disregard unknown events', function () {
         var eventStore = new EventStore();
 
-        eventStore.save({event: 'BeebAdded', data: {date: new Date(), content: 'welcome to beebstream'}})
-        eventStore.save({event: 'BeebTweeted', data: {date: new Date(), content: 'awesome'}})
-        eventStore.save({event: 'BeebAdded', data: {date: new Date(), content: 'yay'}})
+        eventStore.save({event: 'BeebAdded', data: {id: 1, date: new Date(), content: 'welcome to beebstream'}})
+        eventStore.save({event: 'BeebTweeted', data: {id: 2, date: new Date(), content: 'awesome'}})
+        eventStore.save({event: 'BeebAdded', data: {id: 3, date: new Date(), content: 'yay'}})
 
         var beebStream = new BeebStream(eventStore.getAllEvents());
 
         assert.deepEqual([
-            {date: new Date(), content: 'welcome to beebstream'},
-            {date: new Date(), content: 'yay'}
+            {id: 1, date: new Date(), content: 'welcome to beebstream'},
+            {id: 3, date: new Date(), content: 'yay'}
         ], beebStream.toArray());
     });
 });
